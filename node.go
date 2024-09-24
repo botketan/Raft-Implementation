03b7c6edbf988c1fdedb276cde36c99c1f87e3b8
dhaneshragu <dhanesh.v@iitg.ac.in>
2024-09-25 02:27:07 +0530

    protos and grpc

---
+++ node.go
@@ -0,0 +1,130 @@
+package raft
+
+import (
+	"fmt"
+	"net"
+	pb "raft/protos"
+	"sync"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+// Struct to store the information about the peer and the gRPC clients
+type peer struct {
+	conn   *grpc.ClientConn
+	client pb.RaftClient
+}
+
+type peers struct {
+	peerMap map[string]*peer
+	mu      sync.Mutex
+}
+
+// Initialise the peers and return an instance
+func initPeers() *peers {
+	return &peers{
+		peerMap: make(map[string]*peer),
+	}
+}
+
+// gets a peer client of an address mentioned, if it doesn't exist it will be created
+func (p *peers) getPeerClient(address string) (pb.RaftClient, error) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	if peer, ok := p.peerMap[address]; ok {
+		return peer.client, nil
+	}
+
+	// Create a new client with that adress if it doesn't exits
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, fmt.Errorf("error establishing connection to a Raft Peer: %w", err)
+	}
+	p.peerMap[address].conn = conn
+	p.peerMap[address].client = pb.NewRaftClient(conn)
+	return p.peerMap[address].client, nil
+}
+
+// Close all peer connections - cleanup function
+func (p *peers) closeAllPeerConn() {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	for addr, peer := range p.peerMap {
+		peer.conn.Close()
+		delete(p.peerMap, addr)
+	}
+}
+
+type node struct {
+	address  net.Addr     // Address of this server to make RPC calls
+	mu       sync.RWMutex // Mutex to ensure concurrency
+	server   *grpc.Server // gRPC server for nodes
+	peerList *peers       // Struct to manage the peers
+	running  bool         // Is the server running?
+	pb.UnimplementedRaftServer
+}
+
+func initNode(addr string) (*node, error) {
+	netAddr, err := net.ResolveTCPAddr("tcp", addr)
+	if err != nil {
+		return nil, fmt.Errorf("error resolving TCP address : %w", err)
+	}
+	return &node{
+		address:  netAddr,
+		peerList: initPeers(),
+	}, nil
+}
+
+// Start the node and receive RPCs
+func (n *node) Start() error {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
+	if n.running {
+		return nil
+	}
+
+	list, err := net.Listen(n.address.Network(), n.address.String())
+	if err != nil {
+		return fmt.Errorf("error starting the raft node: %w", err)
+	}
+
+	// Intialise the new gRPC server and register it
+	n.server = grpc.NewServer()
+	pb.RegisterRaftServer(n.server, n)
+
+	go n.server.Serve(list)
+	n.running = true
+	return nil
+}
+
+func (n *node) Shutdown() error {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+	defer n.peerList.closeAllPeerConn()
+
+	if !n.running {
+		return nil
+	}
+
+	n.running = false
+
+	stopped := make(chan interface{})
+	go func() {
+		n.server.GracefulStop()
+		close(stopped)
+	}()
+
+	select {
+	case <-time.After(350 * time.Millisecond):
+		n.server.Stop()
+	case <-stopped:
+		n.server.Stop()
+	}
+
+	return nil
+}
